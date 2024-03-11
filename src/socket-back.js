2a7a2862d5aed2a1c9da6f1bcb4964ca53ce10be
Murilo <muriloazarias97@gmail.com>
2024-03-11 02:09:24 -0300

    Avancando na comunicacao: criando salas de socket e guardando dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,13 +1,44 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de node..."
+    },
+    {
+        nome: "Socket.IO",
+        texto: "texto de socket.io..."
+    },
+];
+
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
 
-    socket.on("texto_editor", (texto) => {
-        socket.broadcast.emit("texto_editor_clientes", texto);
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento);
+
+        console.log(documento);
+
+        socket.join(nomeDocumento);
+    })
+    
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
     }),
     socket.on("disconnect", (motivo) => {
         console.log(`Cliente "${socket.id}" desconectado!
         Motivo: ${motivo}`);
       });
 });
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome
+    });
+
+    return documento;
+}
